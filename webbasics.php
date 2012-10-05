9a9c59460ca12260662d1630aadf4edfcb30d101
Taddeus Kroes <taddeuskroes@hotmail.com>
2012-10-05 23:15:21 +0200

    Added base include file that includes the entire library

---
+++ webbasics.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Include file for WebBasics library.
+ * 
+ * @author Taddeus Kroes
+ * @date 05-10-2012
+ * @since 0.2
+ */
+
+// @codeCoverageIgnoreStart
+require_once 'logger.php';
+require_once 'base.php';
+require_once 'autoloader.php';
+require_once 'collection.php';
+require_once 'router.php';
+require_once 'template.php';
+// @codeCoverageIgnoreEnd
+
+?>
